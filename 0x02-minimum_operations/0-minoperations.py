3e714bb27b71e198e8f008c9952d016016bed860
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-05-10 03:56:09 +0100

    Create 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''min operations to write n H using copy all and paste'''
+
+def minOperations(n){
+  num = 0;
+  while(num < n){
+    if(){
+
+  }
+else{
+    num +=1
+}
+  }
+}
