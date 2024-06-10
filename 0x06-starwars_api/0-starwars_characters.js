05c4b8a6fffc66e3271dbe47182b044b1ed0d813
Youness Boumlik <youness@Ubuntu.myguest.virtualbox.org>
2024-06-10 19:03:06 +0100

    asynch / promises

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,10 +1,38 @@
-#!/usr/bin/node
-
 const request = require('request');
 
-request('https://swapi-api.alx-tools.com/api/films/3', function (error, response, data) {
+function printingCharacters(character) {
+    return new Promise((resolve, reject) => {
+        request(character, function (error, response, datachar) {
+            if(error) {
+                console.log('error:', error);
+                reject(error);
+            }
+            else if(response.statusCode !== 200) {
+                console.log('statusCode:', response && response.statusCode);
+                reject(new Error('Invalid status code: ' + response.statusCode));
+            }
+            else if(!datachar) {
+                console.log('No data returned');
+                reject(new Error('No data returned'));
+            }
+            else {
+                let char = JSON.parse(datachar);
+                console.log(char.name);
+                resolve(char.name);
+            }
+        });
+    });
+}
 
+request('https://swapi-api.alx-tools.com/api/films/3', async function (error, response, data) {
+    let jsondata = JSON.parse(data);
+    let characters = jsondata.characters;
 
-console.log(data);
-
+    for (let character of characters) {
+        try {
+            await printingCharacters(character);
+        } catch (error) {
+            console.error('Error printing character:', error);
+        }
+    }
 });
