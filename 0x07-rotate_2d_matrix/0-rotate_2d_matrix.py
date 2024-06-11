855dbdc5cea7d89a837530f026bf3207353c98ec
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-11 05:39:13 +0100

    task0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,10 +4,10 @@ Rotate 2D Matrix
 """
 
 def rotate_2d_matrix(matrix):
-        """rotate two dimension matrix 90 degrees clockwise
-    Args:
-        matrix (list[[list]]): a matrix
-    """
+   """rotate two dimension matrix 90 degrees clockwise
+   Args:
+       matrix (list[[list]]): a matrix
+   """
     matrix_copy = [[matrix[i][j] for j in range(0, len(matrix[i]))] for i in range(0, len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
