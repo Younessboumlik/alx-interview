51042214bb391d43260ce73b837e66cc2064559d
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-12 03:52:29 +0100

    fixing style

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -8,8 +8,7 @@ def rotate_2d_matrix(matrix):
     matrix (list[[list]]): a matrix
     """
     matrix_copy = [[matrix[i][j] for j in range(0,
-                                                len(matrix[i]))] for i in range(0,
-                                                                                len(matrix))]
+            len(matrix[i]))] for i in range(0,len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             matrix[i][j] = matrix_copy[len(matrix) - j - 1][i]
