f230448987a7586a5a6c49b045a13542bde6c28d
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-12 04:00:23 +0100

    task 0

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,8 +7,8 @@ def rotate_2d_matrix(matrix):
     Args:
     matrix (list[[list]]): a matrix
     """
-    matrix_copy = [[matrix[i][j] for j in range(0,
-    len(matrix[i]))] for i in range(0, len(matrix))]
+    matrix_copy = [[matrix[i][j] for j in range(0, 
+                    len(matrix[i]))] for i in range(0, len(matrix))]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             matrix[i][j] = matrix_copy[len(matrix) - j - 1][i]
