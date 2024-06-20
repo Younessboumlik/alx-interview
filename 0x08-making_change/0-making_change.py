e9db36618e375668240c43e477f35987ae520514
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-20 02:36:14 +0100

    first try

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def makeChange(coins, total):
+    """Return the minimum number of coins needed to make change for total."""
+    if total <= 0:
+        return 0
+    coins.sort()
+    coins.reverse()
+    print(coins)
+    n = 0
+    for coin in coins:
+        while total >= coin:
+            n += total // coin
+            total = total % coin
+            if(total == 0):
+                return n
+    return -1
