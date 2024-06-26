3c23c9afaa55310d5e22e92bde1eba5aadb3ddd4
Youness Boumlik <youness@Ubuntu.myguest.virtualbox.org>
2024-06-26 19:39:14 +0100

    adding doc

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" The goal is to calculate the perimeter of a single island in a grid, where the grid is represented by a 2D array of integers. """
+""" The goal is to calculate the perimeter of a single island in a grid,
+where the grid is represented by a 2D array of integers. """
 
 
 def island_perimeter(grid):
@@ -9,16 +10,15 @@ def island_perimeter(grid):
         for j in range(len(grid[i])):
             if grid[i][j] == 0:
                 continue
-            if j == 0 or grid[i][j-1] == 0:  
-                    n += 1
-            
-            if j == len(grid[i]) - 1 or grid[i][j+1] == 0:  
-                    n += 1
-            
-            if i == 0 or grid[i-1][j] == 0: 
-                    n += 1
-            
+            if j == 0 or grid[i][j-1] == 0:
+                n += 1
+
+            if j == len(grid[i]) - 1 or grid[i][j+1] == 0:
+                n += 1
+
+            if i == 0 or grid[i-1][j] == 0:
+                n += 1
             if i == len(grid) - 1 or grid[i+1][j] == 0:
-                    n += 1
-                
+                n += 1
+
     return n
