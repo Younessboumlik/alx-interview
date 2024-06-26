cdbf8ad674cd4723fc35072e066a2ee2375c0589
Youness Boumlik <104656844+Younessboumlik@users.noreply.github.com>
2024-06-26 19:27:01 +0100

    tsak 0

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+def island_perimeter(grid):
+    n = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 0:
+                continue
+            if j == 0 or grid[i][j-1] == 0:  
+                    n += 1
+            
+            if j == len(grid[i]) - 1 or grid[i][j+1] == 0:  
+                    n += 1
+            
+            if i == 0 or grid[i-1][j] == 0: 
+                    n += 1
+            
+            if i == len(grid) - 1 or grid[i+1][j] == 0:
+                    n += 1
+                
+    return n
